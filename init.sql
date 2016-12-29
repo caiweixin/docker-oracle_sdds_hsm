928b74c9a647ced5ed40a12768151e10a948ba0e
caiweixin <18857169657@139.com>
2016-12-29 18:37:32 +0800

    Update init.sql
    增加建表和序列的语句

--- init.sql
+++ init.sql
@@ -3,3 +3,17 @@ grant create session to sdds_hsm;
 grant resource to sdds_hsm;
 grant unlimited tablespace to sdds_hsm;
 grant create view to sdds_hsm;
+
+connect sdds_hsm/sdds_hsm;
+CREATE TABLE KEYSET
+(
+	id NUMBER(19,0) NOT NULL,
+	bank_no VARCHAR2(50) NOT NULL,
+	key_index VARCHAR2(10),
+	hsm_index NUMBER(19,0),
+	private_key VARCHAR2(100),
+	public_key VARCHAR2(150) NOT NULL,
+	address VARCHAR2(50) NOT NULL,
+	primary key (id)
+);
+CREATE SEQUENCE KEYSET_SEQ START WITH 1 INCREMENT BY 1;
